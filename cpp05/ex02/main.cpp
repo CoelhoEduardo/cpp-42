d7b95b77dc037114b4dfff121c0ab6e937308f9e
Eduardo Santos <educs98@outlook.com>
2025-10-31 10:40:15 -0300

    improve mock test's

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 21:36:42 by ecoelho-          #+#    #+#             */
-/*   Updated: 2025/10/30 22:55:49 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2025/10/31 10:10:19 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,26 @@ int main() {
         std::cout << "|  MOCK TEST'S  |" << std::endl;
         std::cout << "-----------------" << std::endl << std::endl;
         
-        Bureaucrat bob("Bob", 1);      // Super poderoso
-        Bureaucrat jim("Jim", 150);    // Fraquinho
+        Bureaucrat bob("Bob", 1);
+        Bureaucrat jim("Jim", 150);
         
+        std::cout << "=== Test 1: Bob executed Shrubbery Creation (SUCCESS ✅) ===" << std::endl << std::endl;
         ShrubberyCreationForm shrub("home");
-        
-        // Cenário 1: Sucesso
         bob.signForm(shrub);
-        bob.executeForm(shrub);  // ✅ "Bob executed Shrubbery Creation"
-        
-        // Cenário 2: Não assinado
+        bob.executeForm(shrub);
+        std::cout << std::endl;
+        std::cout << "=== Test 2: Jim couldn't execute... Form is not signed yet (FAILED ❌) ===" << std::endl << std::endl;
         RobotomyRequestForm robot("Bender");
-        jim.executeForm(robot);  // ❌ "Jim couldn't execute... Form is not signed yet"
-        
-        // Cenário 3: Grade insuficiente
+        jim.executeForm(robot);
+        std::cout << std::endl;
+        std::cout << "=== Test 3: Grade is not enough ===" << std::endl << std::endl;
         PresidentialPardonForm pardon("Arthur");
         bob.signForm(pardon);
-        jim.executeForm(pardon);  // ❌ "Jim couldn't execute... grade too low"
-        bob.executeForm(pardon);  // ✅ "Bob executed Presidential Pardon"
+        std::cout << std::endl;
+        jim.executeForm(pardon);
+        std::cout << std::endl;
+        bob.executeForm(pardon);
+        std::cout << std::endl;
         
     } catch (std::exception & e) {
         std::cerr << "Error: " << e.what() << std::endl;
